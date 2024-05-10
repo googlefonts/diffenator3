d2b3cbb76d262c7d6da805f45d421053014dd913
Simon Cozens <simon@simon-cozens.org>
2024-05-10 14:43:16 +0100

    New docs build

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -55,9 +55,6 @@
 /******/ 		"../pkg/diffenator3_bg.wasm": function() {
 /******/ 			return {
 /******/ 				"./diffenator3_bg.js": {
-/******/ 					"__wbindgen_object_drop_ref": function(p0i32) {
-/******/ 						return installedModules["../pkg/diffenator3_bg.js"].exports["__wbindgen_object_drop_ref"](p0i32);
-/******/ 					},
 /******/ 					"__wbg_new_abda76e883ba8a5f": function() {
 /******/ 						return installedModules["../pkg/diffenator3_bg.js"].exports["__wbg_new_abda76e883ba8a5f"]();
 /******/ 					},
@@ -67,8 +64,8 @@
 /******/ 					"__wbg_error_f851667af71bcfc6": function(p0i32,p1i32) {
 /******/ 						return installedModules["../pkg/diffenator3_bg.js"].exports["__wbg_error_f851667af71bcfc6"](p0i32,p1i32);
 /******/ 					},
-/******/ 					"__wbindgen_throw": function(p0i32,p1i32) {
-/******/ 						return installedModules["../pkg/diffenator3_bg.js"].exports["__wbindgen_throw"](p0i32,p1i32);
+/******/ 					"__wbindgen_object_drop_ref": function(p0i32) {
+/******/ 						return installedModules["../pkg/diffenator3_bg.js"].exports["__wbindgen_object_drop_ref"](p0i32);
 /******/ 					}
 /******/ 				}
 /******/ 			};
@@ -171,7 +168,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/diffenator3_bg.wasm":"255ed0e08b1b0bb0136c"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/diffenator3_bg.wasm":"7d6c911eb3401ec8681d"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
