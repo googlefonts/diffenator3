78445665d9eaff10319aa9d9145746709ecf0979
Simon Cozens <simon@simon-cozens.org>
2024-05-13 13:33:36 +0100

    Work in production mode

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,16 +1,20 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
 module.exports = {
   entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "..", "docs"),
-    filename: "bootstrap.js",
+    filename: "bootstrap.js"
   },
-  // mode: "development",
+  mode: "production",
   plugins: [
     new CopyWebpackPlugin([
         'index.html',
+        'AND-Regular.ttf'
        ]
     )
   ],
