7a866c64584d67bbff268d16b90ee68b40a40073
Simon Cozens <simon@simon-cozens.org>
2025-10-30 14:29:29 +0000

    Better namemap implementation

--- ttj/src/namemap.rs
+++ ttj/src/namemap.rs
@@ -1,38 +1,71 @@
-use read_fonts::TableProvider;
+// Stolen from fontc/otl-normalizer
+
+use std::collections::{BTreeMap, HashMap};
+
+use read_fonts::{
+    tables::cmap::{CmapSubtable, EncodingRecord, PlatformId},
+    TableProvider,
+};
 use skrifa::{FontRef, GlyphId, GlyphId16};
 
-fn gid_to_name<'a>(font: &impl TableProvider<'a>, gid: GlyphId) -> String {
-    if let Ok(gid16) = TryInto::<GlyphId16>::try_into(gid) {
-        if let Ok(Some(name)) = font
-            .post()
-            .map(|post| post.glyph_name(gid16).map(|x| x.to_string()))
-        {
-            return name;
-        }
-    }
-    format!("gid{:}", gid)
+/// Given a `char`, returns the postscript name for that `char`s glyph,
+/// if one exists in the aglfn.
+fn glyph_name_for_char(chr: char) -> Option<String> {
+    fontdrasil::agl::agl_name_for_char(chr).map(Into::into)
 }
 
 /// A map from glyph IDs to glyph names
 #[derive(Debug, Clone)]
-pub struct NameMap(Vec<String>);
+pub struct NameMap(BTreeMap<GlyphId, String>);
 
 impl NameMap {
     /// Generate a new NameMap from a font
     pub fn new(font: &FontRef) -> Self {
-        let num_glyphs = font.maxp().unwrap().num_glyphs();
-        let mut mapping = Vec::with_capacity(num_glyphs as usize);
-        for gid in 0..num_glyphs {
-            mapping.push(gid_to_name(font, GlyphId::new(gid as u32)));
-        }
-        Self(mapping)
+        let num_glyphs = font.maxp().map(|x| x.num_glyphs()).unwrap_or(0);
+        let reverse_cmapping = reverse_cmap(font);
+        let post = font.post().ok();
+        let name_map = (1..num_glyphs)
+            .map(move |gid| {
+                let gid = GlyphId16::new(gid);
+                // first check post, then do fallback
+                if let Some(name) = post
+                    .as_ref()
+                    .and_then(|post| post.glyph_name(gid).map(|x| x.to_string()))
+                {
+                    return (gid.into(), name);
+                }
+                // fallback to unicode or gid
+                let name = match reverse_cmapping
+                    .as_ref()
+                    .and_then(|cmap| cmap.get(&gid))
+                    .and_then(|cp| char::from_u32(*cp))
+                {
+                    Some(codepoint) => match glyph_name_for_char(codepoint) {
+                        Some(name) => name.to_string(),
+                        // we have a codepoint but it doesn't have a name:
+                        None => {
+                            let raw = codepoint as u32;
+                            if raw <= 0xFFFF {
+                                format!("uni{raw:04X}")
+                            } else {
+                                format!("u{raw:X}")
+                            }
+                        }
+                    },
+                    // we have no codepoint, just use glyph ID
+                    None => format!("glyph.{:05}", gid.to_u16()),
+                };
+                (GlyphId::from(gid), name)
+            })
+            .collect::<BTreeMap<GlyphId, _>>();
+        Self(name_map)
     }
 
     /// Get the name of a glyph
     pub fn get(&self, gid: impl Into<GlyphId>) -> String {
         let gid: GlyphId = gid.into();
         self.0
-            .get(gid.to_u32() as usize)
+            .get(&gid)
             .map(|n| n.to_string())
             .unwrap_or_else(|| format!("gid{}", gid))
     }
@@ -58,4 +91,46 @@ impl NameMap {
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
+}
+
+fn reverse_cmap(font: &FontRef) -> Option<HashMap<GlyphId16, u32>> {
+    // <https://github.com/fonttools/fonttools/blob/6fa1a76e061c2e84243d8cac/Lib/fontTools/ttLib/tables/_c_m_a_p.py#L334>
+    fn is_unicode(record: &&EncodingRecord) -> bool {
+        record.platform_id() == PlatformId::Unicode
+            || record.platform_id() == PlatformId::Unicode
+                && [0, 1, 10].contains(&record.encoding_id())
+    }
+
+    let cmap = font.cmap().ok()?;
+    let offset_data = cmap.offset_data();
+
+    let mut reverse_cmap = HashMap::new();
+
+    let mut add_to_map = |args: (u32, GlyphId16)| {
+        // because multiple glyphs may map to the same codepoint,
+        // we always use the lowest codepoint to determine the name.
+        let val = reverse_cmap.entry(args.1).or_insert(args.0);
+        *val = args.0.min(*val);
+    };
+
+    for subtable in cmap
+        .encoding_records()
+        .iter()
+        .filter(is_unicode)
+        .map(|rec| rec.subtable(offset_data).unwrap())
+    {
+        match subtable {
+            CmapSubtable::Format4(subtable) => subtable
+                .iter()
+                .map(|(unicode, gid)| (unicode, GlyphId16::try_from(gid).unwrap()))
+                .for_each(&mut add_to_map),
+            CmapSubtable::Format12(subtable) => subtable
+                .iter()
+                .map(|(unicode, gid)| (unicode, GlyphId16::try_from(gid).unwrap()))
+                .for_each(&mut add_to_map),
+            _ => (),
+        }
+    }
+
+    Some(reverse_cmap)
 }
